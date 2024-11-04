e6fc924c7f587682757d1508698d47a4534e05f2
constantin9845 <constantin.y@outlook.com>
2024-11-03 21:59:52 -0800

    test

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ command = ""
 
 if platform.system() == 'Linux':
     # Command to open a new terminal and start counting
-    command = "gnome-terminal -- bash -c 'for i in {1..1000}; do echo $i; sleep 1; done'"
+    command = "gnome-terminal -- bash -c 'for i in {1..1000}; do echo $i; sleep 1; clear; done'"
 elif platform.system() == 'Windows':
-    command = "start cmd /k \"for /L %i in (1,1,1000) do (echo %i & timeout /t 1)\""
+    command = "start cmd /k \"for /L %i in (1,1,1000) do (echo %i & timeout /t & cls 1)\""
 
 
 # Execute the command
